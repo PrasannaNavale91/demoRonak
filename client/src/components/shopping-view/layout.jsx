13fc64cdc1684823225051605990b2955740b331
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-18 15:27:11 +0530

    change done

--- client/src/components/shopping-view/layout.jsx
+++ client/src/components/shopping-view/layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import ShoppingHeader from "./header";
 
-function ShoppingLayout() {
+function ShoppingLayout({ children}) {
   return (
     <div className="flex flex-col bg-white overflow-hidden">
       {/* common header */}
       <ShoppingHeader />
+      { children }
       <main className="flex flex-col w-full">
         <Outlet />
       </main>
