f0f405b8e00cb29cd5b35f7a40896d1867d7a5d8
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-23 16:22:52 +0530

    change done

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -4,7 +4,7 @@ import { loginFormControls } from "@/config";
 import { loginUser } from "@/store/auth-slice";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const initialState = {
   email: "",
@@ -15,12 +15,14 @@ function AuthLogin() {
   const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const { toast } = useToast();
+  const navigate = useNavigate();
 
   function onSubmit(event) {
     event.preventDefault();
 
     dispatch(loginUser(formData)).then((data) => {
       if (data?.payload?.success) {
+        navigate("/shop/home");
         toast({
           title: data?.payload?.message,
         });
