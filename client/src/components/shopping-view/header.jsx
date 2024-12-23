00366c0789152c36d4c4794af3d1076f59724bd4
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-23 16:56:10 +0530

    change done

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -23,6 +23,7 @@ import UserCartWrapper from "./cart-wrapper";
 import { useEffect, useState } from "react";
 import { fetchCartItems } from "@/store/shop/cart-slice";
 import { Label } from "../ui/label";
+import Logo from "../../assets/logo.png";
 
 function MenuItems() {
   const navigate = useNavigate();
@@ -155,8 +156,7 @@ function ShoppingHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
-          <HousePlug className="h-6 w-6" />
-          <span className="font-bold">Ecommerce</span>
+          <img src={Logo} alt="" className="p-1 w-2 h-2" />
         </Link>
         <Sheet>
           <SheetTrigger asChild>
