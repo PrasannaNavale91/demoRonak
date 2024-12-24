1227340d23e6ea81af82f4bb26ad4ef2a5efa475
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-24 17:50:01 +0530

    change done

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -9,7 +9,7 @@ function ShoppingProductTile({
   handleAddtoCart,
 }) {
   return (
-    <Card className="w-full mx-auto">
+    <Card className="w-72 mx-6">
       <div onClick={() => handleGetProductDetails(product?._id)}>
         <div className="relative">
           <img
