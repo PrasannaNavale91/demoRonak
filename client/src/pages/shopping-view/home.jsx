3481364376f126ea85ad65099ad9c2d7c37fbf2d
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-24 17:09:48 +0530

    change done

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -226,7 +226,7 @@ function ShoppingHome() {
       </section>
 
       <section className="py-12">
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto px-4 relative overflow-hidden">
           <h2 className="text-3xl font-bold text-center mb-8">
             Feature Products
           </h2>
@@ -240,6 +240,7 @@ function ShoppingHome() {
                     product={productItem}
                     handleAddtoCart={handleAddtoCart}
                     key={index}
+                    className="w-[280px]"
                   />
                 ))
               : null}
