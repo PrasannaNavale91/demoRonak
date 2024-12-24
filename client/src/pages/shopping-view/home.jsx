22f7a5412ad4337baea3a3572f65654c2f6aabc7
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-24 17:13:34 +0530

    change done

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -240,7 +240,7 @@ function ShoppingHome() {
                     product={productItem}
                     handleAddtoCart={handleAddtoCart}
                     key={index}
-                    className="w-[280px]"
+                    className="w-72"
                   />
                 ))
               : null}
