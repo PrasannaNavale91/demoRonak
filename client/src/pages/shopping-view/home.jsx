3c2e23c1d75df54c3646ff41673fd898f7118f5f
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-24 17:22:49 +0530

    change done

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -240,7 +240,7 @@ function ShoppingHome() {
                     product={productItem}
                     handleAddtoCart={handleAddtoCart}
                     key={index}
-                    className="flex-shrink-0 w-full h-full flex justify-center items-center"
+                    className="flex-shrink-0 w-72 h-full flex justify-center items-center gap-4"
                   />
                 ))
               : null}
