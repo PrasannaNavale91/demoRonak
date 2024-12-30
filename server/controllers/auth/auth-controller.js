80111a785f2d4a3cdff72ceb3f5c7ce83e254d57
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-12-30 18:43:36 +0530

    change done

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
-const sendEmail = require("./mail-controller");
+const sendEmail = require("../../middleware/Email");
 
 //register
 const registerUser = async (req, res) => {
