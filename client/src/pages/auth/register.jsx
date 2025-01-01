e7d0ea8167f7b66027d4b70d9191ab6268da07e6
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-01-01 17:59:48 +0530

    change done

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -18,18 +18,6 @@ function AuthRegister() {
   const navigate = useNavigate();
   const { toast } = useToast();
 
-  async function sendWelcomeEmail(email, userName) {
-    try {
-      const response = await axios.post("https://ecommerce-d3qt.onrender.com/api/auth/register/send-notification-mail", {
-        email,
-        name: userName,
-      });
-      console.log(response.data.message);
-    } catch (error) {
-      console.error("Failed to send email:", error);
-    }
-  }
-
   function onSubmit(event) {
     event.preventDefault();
     dispatch(registerUser(formData)).then((data) => {
@@ -37,8 +25,6 @@ function AuthRegister() {
         toast({
           title: data?.payload?.message,
         });
-        
-        sendWelcomeEmail(formData.email, formData.userName);
 
         navigate("/auth/login");
       } else {
