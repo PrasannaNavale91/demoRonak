14e910c69139d40feb5077afcf3acc18e2bb68cf
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-01-01 17:24:58 +0530

    change done

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -44,7 +44,8 @@ function CommonForm({
                 [getControlItem.name]: event.target.value,
               })
             }
-          />
+          />,
+          <span onClick={handleToggle} className='ml-[-30px] text-lg cursor-pointer'>{show ? <EyeOff /> : <Eye /> }</span>
         );
         break;
       case "select":
@@ -105,8 +106,7 @@ function CommonForm({
                 [getControlItem.name]: event.target.value,
               })
             }
-          />,
-          <span onClick={handleToggle} className='ml-[-30px] text-lg cursor-pointer'>{show ? <EyeOff /> : <Eye /> }</span>
+          />
         );
         break;
     }
