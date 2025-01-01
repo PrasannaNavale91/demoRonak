96e0f6233a98e47d885de0f7bbf61196bd42d056
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-01-01 19:09:31 +0530

    change done

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -9,9 +9,12 @@ const registerUser = async (req, res) => {
 
   try {
     const checkUser = await User.findOne({ email });
-    if (password !== confirmPassword) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
+    if (password !== confirmPassword)
+      return res.json({
+        success: false,
+        message: "Password doesn't match! Please Check..",
+      });
+    
     if (checkUser)
       return res.json({
         success: false,
