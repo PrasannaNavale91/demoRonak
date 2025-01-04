b72f98eeec1e90dd529efd10088833196779b394
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-01-04 21:10:45 +0530

    change done

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const razorpayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: RAZORPAY_KEY_SECRET
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 const createOrder = async (req, res) => {
