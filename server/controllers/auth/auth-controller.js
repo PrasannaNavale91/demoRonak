c361f27d91af3cf7b8d0ff644e357e2b3e29e93f
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-01-07 21:36:33 +0530

    change done

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -136,17 +136,22 @@ const subscribeUser = async (req, res) => {
 
 //auth middleware
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token)
-    return res.status(401).json({
-      success: false,
-      message: "Unauthorised user!",
-    });
-
   try {
+    const token = req.cookies.token;
+    if (!token){
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorised user!",
+      });
+    }
     const decoded = jwt.verify(token, process.env.JWT_TOKEN);
-    req.user = decoded;
-    next();
+    
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ user });
   } catch (error) {
     res.status(401).json({
       success: false,
