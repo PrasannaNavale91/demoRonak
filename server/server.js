6ffa4358a95672a77a1a94f16e606c1bb5bc56c6
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-01-08 21:09:22 +0530

    change done

--- server/server.js
+++ server/server.js
@@ -24,14 +24,12 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
+app.use(cors({
   origin: "https://ecommerce-d3qt.onrender.com",
   methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: [ "Content-Type", "Authorization"],
+  allowedHeaders: [ "Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma",],
   credentials: true,
-};
-
-app.use(cors(corsOptions));
+}));
 
 app.use(cookieParser());
 app.use(express.json());
