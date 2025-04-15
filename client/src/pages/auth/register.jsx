a78fca9baaeb7b398ff7e73cef80489d95b9f0bd
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-04-15 12:40:25 +0530

    new changes

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -57,6 +57,15 @@ function AuthRegister() {
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
           Create new account
         </h1>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+      <div className="text-center">
         <p className="mt-2">
           Already have an account
           <Link
@@ -67,13 +76,6 @@ function AuthRegister() {
           </Link>
         </p>
       </div>
-      <CommonForm
-        formControls={registerFormControls}
-        buttonText={"Sign Up"}
-        formData={formData}
-        setFormData={setFormData}
-        onSubmit={onSubmit}
-      />
     </div>
   );
 }
--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -121,7 +121,7 @@ const sendOtp = async (req, res) => {
     }
 
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    await OPT.create({ email, otp });
+    await Otp.create({ email, otp });
 
     const msg = {
       to: email,
