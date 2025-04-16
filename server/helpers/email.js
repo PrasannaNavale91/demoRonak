1b3f88447f27774efa3b8b36d5c43b1c263c5d70
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-04-16 11:28:44 +0530

    new changes

--- server/helpers/email.js
+++ server/helpers/email.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async ({ to, name = 'User', otp}) => {
+const sendEmail = async ( to, name = 'User', otp) => {
   try {
     const msg = {
       to,
