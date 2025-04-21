8dbfe426f1c72d5f42a0a12772787a02c160bfe7
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-04-21 18:32:16 +0530

    new changes

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -181,7 +181,7 @@ function ShoppingListing() {
             </DropdownMenu>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 p-2 w-[200px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-2">
           {productList && productList.length > 0
             ? productList.map((productItem) => (
                 <ShoppingProductTile
