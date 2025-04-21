7da8f75931e2ec07f65a7d60dd18cf78744ca88d
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-04-21 18:28:40 +0530

    new changes

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -204,7 +204,7 @@ function ShoppingHome() {
       <section className="py-12 bg-gray-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">Check Out Our Collection</h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {collectionWithIcon.map((collectionItem) => (
               <Card
                 onClick={() => handleNavigateToListingPage(collectionItem, "collection")}
