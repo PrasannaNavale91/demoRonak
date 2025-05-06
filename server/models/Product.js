e0c79ae49c55cd2cb75ff00d918f44c292f57851
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-05-06 19:07:51 +0530

    new changes

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,7 +4,7 @@ const ProductSchema = new mongoose.Schema(
   {
     image: {
       type : String,
-      required : true,
+      required : false,
     },
     title: String,
     description: String,
