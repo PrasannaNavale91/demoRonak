5ad02a80b6f95150c059096c0ee7ab0105581f49
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-05-14 20:04:46 +0530

    new changes

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -91,38 +91,50 @@ function HeaderRightContent() {
       {isAuthenticated ? (
         <>
           <Sheet open={openCartSheet} onOpenChange={() => setOpenCartSheet(false)}>
-            <Button
-              onClick={() => setOpenCartSheet(true)}
-              variant="outline"
-              size="icon"
-              className="relative"
-            >
-              <HeartIcon className="w-6 h-6" />
-              <span className="absolute top-[-5px] right-[2px] font-bold text-sm">
-                {wishlistItems?.items?.length || 0}
-              </span>
-              <span className="sr-only">User Wishlist</span>
-            </Button>
-            <Button
-              onClick={() => setOpenCartSheet(true)}
-              variant="outline"
-              size="icon"
-              className="relative"
-            >
-              <ShoppingCart className="w-6 h-6" />
-              <span className="absolute top-[-5px] right-[2px] font-bold text-sm">
-                {cartItems?.items?.length || 0}
-              </span>
-              <span className="sr-only">User cart</span>
-            </Button>
-            <UserCartWrapper
-              setOpenCartSheet={setOpenCartSheet}
-              cartItems={
-                cartItems && cartItems.items && cartItems.items.length > 0
-                  ? cartItems.items
-                  : []
-              }
-            />
+            <div>
+              <Button
+                onClick={() => setOpenCartSheet(true)}
+                variant="outline"
+                size="icon"
+                className="relative"
+              >
+                <HeartIcon className="w-6 h-6" />
+                <span className="absolute top-[-5px] right-[2px] font-bold text-sm">
+                  {wishlistItems?.items?.length || 0}
+                </span>
+                <span className="sr-only">User Wishlist</span>
+              </Button>
+              <UserCartWrapper
+                setOpenCartSheet={setOpenCartSheet}
+                cartItems={
+                  wishlistItems && wishlistItems.items && wishlistItems.items.length > 0
+                    ? wishlistItems.items
+                    : []
+                }
+              />
+            </div>
+            <div>
+              <Button
+                onClick={() => setOpenCartSheet(true)}
+                variant="outline"
+                size="icon"
+                className="relative"
+              >
+                <ShoppingCart className="w-6 h-6" />
+                <span className="absolute top-[-5px] right-[2px] font-bold text-sm">
+                  {cartItems?.items?.length || 0}
+                </span>
+                <span className="sr-only">User cart</span>
+              </Button>
+              <UserCartWrapper
+                setOpenCartSheet={setOpenCartSheet}
+                cartItems={
+                  cartItems && cartItems.items && cartItems.items.length > 0
+                    ? cartItems.items
+                    : []
+                }
+              />
+            </div>
           </Sheet>
 
           <DropdownMenu>
