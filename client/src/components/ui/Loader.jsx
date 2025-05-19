8c71f122a4a7d2e843802a6a067fd7ae84210969
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2025-05-19 17:23:37 +0530

    new changes

--- client/src/components/ui/Loader.jsx
+++ client/src/components/ui/Loader.jsx
@@ -2,7 +2,7 @@ import Logo from "../../assets/logo.png"
 
 function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen bg-white-400">
+    <div className="flex items-center justify-center h-screen bg-gray-400">
       <img
         src={Logo}
         alt="Loading..."
